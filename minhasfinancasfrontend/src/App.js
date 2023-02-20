4687a39e64e270b849f51a603f66c5c6fc796424
Juliana Sidegum <jsidegum@gmail.com>
2023-02-20 08:29:54 -0300

    Apenas ajuste de sintaxe, trocar function por class

--- minhasfinancasfrontend/src/App.js
+++ minhasfinancasfrontend/src/App.js
@@ -1,9 +1,14 @@
-function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
 }
 
 export default App;
