6de1495f5e3055e30b42f49c453a67bfda0b5110
Juliana Sidegum <jsidegum@gmail.com>
2023-02-20 09:08:44 -0300

    104. Trabalhando com estado e comportamento nos componentes

--- minhasfinancasfrontend/src/App.js
+++ minhasfinancasfrontend/src/App.js
@@ -2,10 +2,41 @@ import React from 'react';
 
 class App extends React.Component {
 
+  state = {
+    num1: null,
+    num2: null,
+    resultado: null
+  }
+
+  somar = () => { this.setState({ resultado: parseFloat(this.state.num1) + parseFloat(this.state.num2) }) };
+
   render() {
     return (
       <div>
-        Hello World!
+
+        <div>
+          <div>
+            <label>Numero 1:</label>
+            <input type="number" value={this.state.num1} onChange={(e) => this.setState({ num1: e.target.value })} />
+          </div>
+
+          <div>
+            <label>Numero 2:</label>
+            <input type="number" value={this.state.num2} onChange={(e) => this.setState({ num2: e.target.value })} />
+          </div>
+
+          <button
+            onClick={this.somar}
+          >
+            SOMAR
+          </button>
+
+          <h1>
+            {this.state.resultado}
+          </h1>
+
+        </div>
+
       </div>
     )
   }
