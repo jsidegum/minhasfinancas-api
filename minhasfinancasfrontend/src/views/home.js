7c2aa331e90bca23feded15fdf60619cc461f911
Juliana Sidegum <jsidegum@gmail.com>
2023-04-03 13:41:55 -0300

    Calculando o saldo apenas com status Efetivado para ser apresentado na view home

--- minhasfinancasfrontend/src/views/home.js
+++ minhasfinancasfrontend/src/views/home.js
@@ -28,7 +28,7 @@ class Home extends Component {
             <div className="jumbotron">
                 <h1 className="display-3">Bem vindo!</h1>
                 <p className="lead">Esse é seu sistema de finanças.</p>
-                <p className="lead">Seu saldo para o mês atual é de R$ {this.state.saldo}</p>
+                <p className="lead">Seu saldo efetivado é de R$ {this.state.saldo}</p>
                 <hr className="my-4" />
                 <p>E essa é sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
                 <p className="lead">
--- src/main/java/com/jsidegum/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/jsidegum/minhasfinancas/model/repository/LancamentoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.jsidegum.minhasfinancas.model.entity.Lancamento;
+import com.jsidegum.minhasfinancas.model.enums.StatusLancamento;
 import com.jsidegum.minhasfinancas.model.enums.TipoLancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
@@ -15,6 +16,10 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
 				 + "from Lancamento l join l.usuario u "
 				 + "where u.id = :idUsuario "
 				 	   + "and l.tipo = :tipo "
+				 	  + "and l.status = :status "
 				 	   + "group by u")
-	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
+	BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(
+			@Param("idUsuario") Long idUsuario, 
+			@Param("tipo") TipoLancamento tipo,
+			@Param("status") StatusLancamento status);
 }
