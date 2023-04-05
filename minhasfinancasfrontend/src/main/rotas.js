c44ca249e3f372d26b03a8c0ff809b7259fdf322
Juliana Sidegum <jsidegum@gmail.com>
2023-04-05 12:08:41 -0300

    Aplicando proteção de rotas, apenas usuarios autenticados devem acessar.

--- minhasfinancasfrontend/src/main/rotas.js
+++ minhasfinancasfrontend/src/main/rotas.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route, Switch, HashRouter } from 'react-router-dom';
+import { Route, Switch, HashRouter, Redirect } from 'react-router-dom';
 
 import Home from '../views/home';
 import Login from '../views/login';
@@ -9,16 +9,41 @@ import CadastroLancamento from '../views/lancamentos/cadastro-lancamento';
 import ConsultaLancamentos from '../views/lancamentos/consulta-lancamentos';
 
 
+const isUsuarioAutenticado = () => {
+    return false;
+}
+
+function RotaAutenticada({ component: Component, ...props }) {
+
+    return (
+        <Route {...props} render={(componentProps) => {
+            if (isUsuarioAutenticado()) {
+                return (
+                    <Component {...componentProps} />
+                )
+            } else {
+                return (
+                    <Redirect to={{ pathname: '/login', state: { from: componentProps.location } }} />
+                )
+            }
+        }} />
+    )
+
+}
+
 class Rotas extends Component {
     render() {
         return (
             <HashRouter>
                 <Switch>
-                    <Route path="/home" component={Home} />
+
                     <Route path="/login" component={Login} />
                     <Route path="/cadastro-usuarios" component={CadastroUsuario} />
-                    <Route path="/lancamentos" component={ConsultaLancamentos} />
-                    <Route path="/cadastro-lancamento/:id?" component={CadastroLancamento} />
+
+                    <RotaAutenticada path="/home" component={Home} />
+                    <RotaAutenticada path="/lancamentos" component={ConsultaLancamentos} />
+                    <RotaAutenticada path="/cadastro-lancamento/:id?" component={CadastroLancamento} />
+
                 </Switch>
             </HashRouter >
         )
