d23a26a910a6b308e9a560b75a310f1470e011ed
Juliana Sidegum <jsidegum@gmail.com>
2023-05-02 13:56:10 -0300

    Configurando autenticação em memória

--- src/main/java/com/jsidegum/minhasfinancas/config/SecurityConfiguration.java
+++ src/main/java/com/jsidegum/minhasfinancas/config/SecurityConfiguration.java
@@ -1,16 +1,32 @@
 package com.jsidegum.minhasfinancas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+		
+		String senhaCodificada = passwordEncoder().encode("admin");
+		
+		auth
+			.inMemoryAuthentication()
+			.withUser("admin")
+			.password(senhaCodificada)
+			.roles("USER");
 	}
 	
 	@Override
