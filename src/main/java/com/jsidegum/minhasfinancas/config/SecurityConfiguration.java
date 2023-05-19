7540c43dcff85b5750603cc97eea6770b47b51a4
Juliana Sidegum <jsidegum@gmail.com>
2023-05-19 15:17:50 -0300

    - Configurando Permissões da API - Configurando o Gerenciador de Autenticação - Implementação do user details service

--- src/main/java/com/jsidegum/minhasfinancas/config/SecurityConfiguration.java
+++ src/main/java/com/jsidegum/minhasfinancas/config/SecurityConfiguration.java
@@ -1,15 +1,23 @@
 package com.jsidegum.minhasfinancas.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.jsidegum.minhasfinancas.service.impl.SecurityUserDetailsService;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private SecurityUserDetailsService userDetailsService;
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -19,21 +27,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		String senhaCodificada = passwordEncoder().encode("admin");
-		
 		auth
-			.inMemoryAuthentication()
-			.withUser("admin")
-			.password(senhaCodificada)
-			.roles("USER");
+			.userDetailsService(userDetailsService)
+			.passwordEncoder(passwordEncoder());
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.csrf().disable()
-			.authorizeRequests().anyRequest().authenticated()
+			.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/api/usuarios/autenticar").permitAll()
+				.antMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
+				.anyRequest().authenticated()
+			.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 				.httpBasic();
 	}
